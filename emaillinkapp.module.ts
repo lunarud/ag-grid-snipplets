2a9f32388110e3fddf055c1c85653a9b9cb8753c
lunarud <rudy.luna@gmail.com>
2025-04-09 04:20:01 -0500

    Create emaillinkapp.module.ts

---
+++ emaillinkapp.module.ts
@@ -0,0 +1,33 @@
+// app.module.ts (updated)
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AgGridModule } from 'ag-grid-angular';
+import { DataGridComponent } from './data-grid.component';
+import { EmailEditorDialogComponent } from './email-editor-dialog.component';
+import { EmailLinkRendererComponent } from './email-link-renderer.component';
+
+@NgModule({
+  declarations: [
+    DataGridComponent,
+    EmailEditorDialogComponent,
+    EmailLinkRendererComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    AgGridModule
+  ],
+  bootstrap: [DataGridComponent]
+})
+export class AppModule { }
