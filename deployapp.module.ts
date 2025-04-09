80b98cae21bc7f7e36945207f8e234b9cf5588b2
lunarud <rudy.luna@gmail.com>
2025-04-09 04:49:35 -0500

    Create deployapp.module.ts

---
+++ deployapp.module.ts
@@ -0,0 +1,33 @@
+// app.module.ts
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { AgGridModule } from 'ag-grid-angular';
+import { DataGridComponent } from './data-grid.component';
+import { BpmnViewerDialogComponent } from './bpmn-viewer-dialog.component';
+import { BpmnLinkRendererComponent } from './bpmn-link-renderer.component';
+import { CamundaService } from './camunda.service';
+
+@NgModule({
+  declarations: [
+    DataGridComponent,
+    BpmnViewerDialogComponent,
+    BpmnLinkRendererComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
+    AgGridModule
+  ],
+  providers: [CamundaService],
+  bootstrap: [DataGridComponent]
+})
+export class AppModule { }
